471c272d83d6f6610942423164b29443e31ef7aa
milk2715093695 <miao_renke@qq.com>
2025-12-30 01:16:40 +0800

    chore(bash): 调整了实验的参数

--- run_ablation.sh
+++ run_ablation.sh
@@ -1,7 +1,7 @@
-python3 src/main.py --model_name AblationStagewiseGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset Grocery_and_Gourmet_Food --c 0.1 --even_layer 2 --odd_layer 3 --epoch 500 --enabled_stage 1
-python3 src/main.py --model_name AblationStagewiseGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset Grocery_and_Gourmet_Food --c 0.1 --even_layer 2 --odd_layer 3 --epoch 500 --enabled_stage 2
-python3 src/main.py --model_name AblationStagewiseGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset Grocery_and_Gourmet_Food --c 0.1 --even_layer 2 --odd_layer 3 --epoch 500 --enabled_stage 3
+python3 src/main.py --model_name AblationStagewiseGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset Grocery_and_Gourmet_Food --c 0.1 --even_layer 4 --odd_layer 3 --epoch 500 --enabled_stage 1
+python3 src/main.py --model_name AblationStagewiseGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset Grocery_and_Gourmet_Food --c 0.1 --even_layer 4 --odd_layer 3 --epoch 500 --enabled_stage 2
+python3 src/main.py --model_name AblationStagewiseGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset Grocery_and_Gourmet_Food --c 0.1 --even_layer 4 --odd_layer 3 --epoch 500 --enabled_stage 3
 
-python3 src/main.py --model_name AblationStagewiseGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset ML_1MTOPK --c 0.1 --even_layer 2 --odd_layer 3 --epoch 500 --enabled_stage 1
-python3 src/main.py --model_name AblationStagewiseGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset ML_1MTOPK --c 0.1 --even_layer 2 --odd_layer 3 --epoch 500 --enabled_stage 2
-python3 src/main.py --model_name AblationStagewiseGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset ML_1MTOPK --c 0.1 --even_layer 2 --odd_layer 3 --epoch 500 --enabled_stage 3
+python3 src/main.py --model_name AblationStagewiseGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset ML_1MTOPK --c 0.1 --even_layer 4 --odd_layer 3 --epoch 500 --enabled_stage 1
+python3 src/main.py --model_name AblationStagewiseGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset ML_1MTOPK --c 0.1 --even_layer 4 --odd_layer 3 --epoch 500 --enabled_stage 2
+python3 src/main.py --model_name AblationStagewiseGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset ML_1MTOPK --c 0.1 --even_layer 4 --odd_layer 3 --epoch 500 --enabled_stage 3
