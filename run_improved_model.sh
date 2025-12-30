10e57c96b73e6b40e2662edc31867606b8b0a39e
milk2715093695 <miao_renke@qq.com>
2025-12-30 20:25:44 +0800

    fix(bash): 修改 bash 脚本运行的实验参数

--- run_improved_model.sh
+++ run_improved_model.sh
@@ -1,2 +1,2 @@
 python3 src/main.py --model_name TargetGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset Grocery_and_Gourmet_Food --c 0.1 --even_layer 4 --odd_layer 3 --epoch 500
-python3 src/main.py --model_name TargetGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset ML_1MTOPK --c 0.1 --even_layer 4 --odd_layer 3 --epoch 500
+python3 src/main.py --model_name TargetGCN --emb_size 64 --lr 1e-4 --l2 0 --dataset ML_1MTOPK --c 0.1 --even_layer 2 --odd_layer 3 --epoch 500
